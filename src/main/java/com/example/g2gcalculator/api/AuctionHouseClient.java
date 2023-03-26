466a9b355b0c378867e0f2969c6b2f55093859db
Alexey <dignitionn@gmail.com>
2023-03-26 16:34:16 +0300

    add get item info request to open feign client

--- src/main/java/com/example/g2gcalculator/api/AuctionHouseClient.java
+++ src/main/java/com/example/g2gcalculator/api/AuctionHouseClient.java
@@ -1,14 +1,14 @@
 package com.example.g2gcalculator.api;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name = "auction-house", url = "https://pricing-api.tradeskillmaster.com")
 public interface AuctionHouseClient {
-    @RequestMapping(method = RequestMethod.GET, value = "/ah/{auctionHouseId}", produces = "application/json")
+    @GetMapping(value = "/ah/{auctionHouseId}", produces = "application/json")
     String getAllAuctionHouseItems(@PathVariable Integer auctionHouseId);
 
-
+    @GetMapping(value = "/ah/{auctionHouseId}/item/{itemId}", produces = "application/json")
+    String getAuctionHouseItem(@PathVariable Integer auctionHouseId, @PathVariable Integer itemId);
 }
