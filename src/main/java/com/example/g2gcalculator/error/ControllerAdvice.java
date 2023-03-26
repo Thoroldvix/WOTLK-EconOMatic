5356cc8c5f49192dddfc584ad47485379d8f912c
Alexey <dignitionn@gmail.com>
2023-03-26 17:11:33 +0300

    add exception handling for feign exception and bad request

--- src/main/java/com/example/g2gcalculator/error/ControllerAdvice.java
+++ src/main/java/com/example/g2gcalculator/error/ControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.example.g2gcalculator.error;
 
+import feign.FeignException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +13,12 @@ public class ControllerAdvice {
     public ResponseEntity<ApiError> handleNotFoundException(NotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiError(HttpStatus.NOT_FOUND.value(), "Not found"));
     }
-
-
+    @ExceptionHandler(FeignException.NotFound.class)
+    public ResponseEntity<ApiError> handleFeignNotFoundException(FeignException.NotFound e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiError(HttpStatus.BAD_REQUEST.value(), "Not found"));
+    }
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiError> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiError(HttpStatus.BAD_REQUEST.value(), "Bad request"));
+    }
 }
