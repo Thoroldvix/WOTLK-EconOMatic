7ae9ee2aea0f972639423226e6dd034b4d967171
Alexey <65923482+Thoroldvix@users.noreply.github.com>
2023-03-27 00:05:11 +0300

    refactor to use realm object instead of id

--- src/main/java/com/example/g2gcalculator/repository/AuctionHouseRepository.java
+++ src/main/java/com/example/g2gcalculator/repository/AuctionHouseRepository.java
@@ -1,12 +1,16 @@
 package com.example.g2gcalculator.repository;
 
 import com.example.g2gcalculator.model.AuctionHouse;
+import com.example.g2gcalculator.model.Realm;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AuctionHouseRepository extends JpaRepository<AuctionHouse, Integer> {
-    List<AuctionHouse> findAllByRealmId(Integer realmId);
+
+    Optional<AuctionHouse> findByRealm(Realm realm);
 }
