83cde7a754715e1a63c37fa588f5634939121aab
Alexey <65923482+Thoroldvix@users.noreply.github.com>
2023-03-27 00:14:33 +0300

    small refactor

--- src/main/java/com/example/g2gcalculator/service/impl/ClassicPriceService.java
+++ src/main/java/com/example/g2gcalculator/service/impl/ClassicPriceService.java
@@ -9,36 +9,34 @@ import com.example.g2gcalculator.model.Realm;
 import com.example.g2gcalculator.repository.ClassicPriceRepository;
 import com.example.g2gcalculator.service.AuctionHouseService;
 import com.example.g2gcalculator.service.PriceService;
+import com.example.g2gcalculator.service.RealmService;
 import com.example.g2gcalculator.service.ScrapingService;
 import lombok.RequiredArgsConstructor;
 
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.convert.DurationUnit;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
 
 import static com.example.g2gcalculator.util.CalculatorUtils.*;
 
 @Service
+@Setter
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class ClassicPriceService implements PriceService {
-
     private final ClassicPriceRepository classicPriceRepository;
-    private final ClassicRealmService classicRealmService;
-
+    private final RealmService classicRealmService;
     private final AuctionHouseService classicAuctionHouseService;
     private final ScrapingService classicScrapingService;
     private final PriceMapper priceMapper;
-    @Value("${g2g.scraping-interval:60}")
-    @DurationUnit(ChronoUnit.MINUTES)
+    @Value("${g2g.scraping-interval:PT1H}")
     private Duration updateFrequency;
     @Value("${g2g.force-update:false}")
     private boolean forceUpdate;
@@ -47,19 +45,19 @@ public class ClassicPriceService implements PriceService {
     @Transactional
     public PriceResponse getPriceForRealm(String realmName) {
         Realm realm = classicRealmService.getRealm(realmName);
-
         Optional<Price> recentPrice = classicPriceRepository.findMostRecentPriceByRealm(realm);
 
         if (recentPrice.isEmpty() || forceUpdate || checkIfOld(recentPrice.get(), updateFrequency)) {
-            return priceMapper.toPriceResponse(updatePrice(realm));
+            return priceMapper.toPriceResponse(updatePriceForRealm(realm));
         } else {
             return priceMapper.toPriceResponse(recentPrice.get());
         }
     }
 
-    protected Price updatePrice(Realm realm) {
+    @Transactional
+    public Price updatePriceForRealm(Realm realm) {
         Price price = classicScrapingService.fetchRealmPrice(realm);
-        realm.setPrice(price);
+        price.setRealm(realm);
         classicPriceRepository.save(price);
         return price;
     }
@@ -74,10 +72,10 @@ public class ClassicPriceService implements PriceService {
     }
 
     @Override
-    @Transactional
     public ItemPriceResponse getPriceForItem(String realmName, Integer itemId) {
         Realm realm = classicRealmService.getRealm(realmName);
-        ItemResponse item = classicAuctionHouseService.getAuctionHouseItem(realm.getAuctionHouse().getId(), itemId);
+        Integer auctionHouseId = realm.getAuctionHouse().getId();
+        ItemResponse item = classicAuctionHouseService.getAuctionHouseItem(auctionHouseId, itemId);
         PriceResponse price = getPriceForRealm(realmName);
         return calculateItemPrice(item, price);
     }
