56bb72e1e06f48fde3781bb32dbaa382c6712b91
Alexey <dignitionn@gmail.com>
2023-03-26 17:46:25 +0300

    add method to specifically return realm response

--- src/main/java/com/example/g2gcalculator/service/RealmService.java
+++ src/main/java/com/example/g2gcalculator/service/RealmService.java
@@ -1,6 +1,7 @@
 package com.example.g2gcalculator.service;
 
 import com.example.g2gcalculator.dto.RealmResponse;
+import com.example.g2gcalculator.model.Realm;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
@@ -8,5 +9,7 @@ import java.util.List;
 public interface RealmService {
     List<RealmResponse> getAllRealms(Pageable pageable);
 
-    RealmResponse getRealm(String realmName);
+    RealmResponse getRealmResponse(String realmName);
+
+    Realm getRealm(String realmName);
 }
