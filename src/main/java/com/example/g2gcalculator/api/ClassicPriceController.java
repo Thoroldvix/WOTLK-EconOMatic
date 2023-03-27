3b8a4824170decce24b1371aa51f11cfdd67efa3
Alexey <65923482+Thoroldvix@users.noreply.github.com>
2023-03-27 23:54:59 +0300

    add optional params for amount of items to calculate price for and to use MinBuyout or not

--- src/main/java/com/example/g2gcalculator/api/ClassicPriceController.java
+++ src/main/java/com/example/g2gcalculator/api/ClassicPriceController.java
@@ -1,14 +1,12 @@
 package com.example.g2gcalculator.api;
 
+import com.example.g2gcalculator.dto.ItemPriceResponse;
 import com.example.g2gcalculator.dto.PriceResponse;
 import com.example.g2gcalculator.service.PriceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,7 +18,7 @@ public class ClassicPriceController {
     private final PriceService classicPriceService;
 
      @GetMapping("/{realmName:\\w+-\\w+}")
-    public ResponseEntity<?> getPriceForRealm(@PathVariable  String realmName) {
+    public ResponseEntity<PriceResponse> getPriceForRealm(@PathVariable  String realmName) {
         return ResponseEntity.ok(classicPriceService.getPriceForRealm(realmName));
     }
 
@@ -30,8 +28,10 @@ public class ClassicPriceController {
          return ResponseEntity.ok(classicPriceService.getAllPricesForRealm(realmName, pageable));
     }
     @GetMapping("/{realmName:\\w+-\\w+}/items/{itemId:^[1-9]\\d*$}")
-    public ResponseEntity<?> getPriceForItem(@PathVariable String realmName,
-                                             @PathVariable int itemId) {
-         return ResponseEntity.ok(classicPriceService.getPriceForItem(realmName, itemId));
+    public ResponseEntity<ItemPriceResponse> getPriceForItem(@PathVariable String realmName,
+                                                             @PathVariable Integer itemId,
+                                                             @RequestParam(required = false, defaultValue = "1") Integer amount,
+                                                             @RequestParam(required = false, defaultValue = "false") Boolean minBuyout) {
+         return ResponseEntity.ok(classicPriceService.getPriceForItem(realmName, itemId, amount, minBuyout));
     }
 }
