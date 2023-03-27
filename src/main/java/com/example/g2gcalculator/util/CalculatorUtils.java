4297e437018fee3143dbe83209675d6ec989f605
Alexey <65923482+Thoroldvix@users.noreply.github.com>
2023-03-27 23:52:13 +0300

    refactor ClassicPriceService

--- src/main/java/com/example/g2gcalculator/util/CalculatorUtils.java
+++ src/main/java/com/example/g2gcalculator/util/CalculatorUtils.java
@@ -1,10 +1,6 @@
 package com.example.g2gcalculator.util;
 
-import com.example.g2gcalculator.error.NotFoundException;
-import com.example.g2gcalculator.model.Faction;
 import com.example.g2gcalculator.model.Price;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -14,9 +10,5 @@ public final class CalculatorUtils {
 
     private CalculatorUtils() { }
 
-    public static boolean checkIfOld(Price recentPrice, Duration priceUpdateInterval) {
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime lastUpdated = recentPrice.getUpdatedAt();
-        return now.isAfter(lastUpdated.plus(priceUpdateInterval));
-    }
+
 }
