859ed168873bd36eae3512d870a4c29a9cc8a446
Alexey <65923482+Thoroldvix@users.noreply.github.com>
2023-03-29 22:59:45 +0300

    add ability to chose which currency to use

--- src/main/java/com/thoroldvix/g2gcalculator/service/ItemPriceCalculator.java
+++ src/main/java/com/thoroldvix/g2gcalculator/service/ItemPriceCalculator.java
@@ -1,11 +1,12 @@
 package com.thoroldvix.g2gcalculator.service;
 
 import com.thoroldvix.g2gcalculator.dto.ItemPriceResponse;
+import com.thoroldvix.g2gcalculator.dto.PriceResponse;
 
 import java.math.BigDecimal;
 
 public interface ItemPriceCalculator {
-    ItemPriceResponse calculatePrice(long itemPrice, BigDecimal price, int amount);
+    ItemPriceResponse calculatePrice(long itemPrice, PriceResponse price, int amount);
 
 
 }
