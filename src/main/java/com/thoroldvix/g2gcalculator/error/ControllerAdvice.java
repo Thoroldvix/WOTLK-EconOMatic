9989a23fdfd7d7421683755b73ecd3833b363a9b
Alexey <65923482+Thoroldvix@users.noreply.github.com>
2023-03-29 18:44:57 +0300

    remove message from apierror

--- src/main/java/com/thoroldvix/g2gcalculator/error/ControllerAdvice.java
+++ src/main/java/com/thoroldvix/g2gcalculator/error/ControllerAdvice.java
@@ -11,7 +11,7 @@ public class ControllerAdvice {
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ApiError> handleNotFoundException(NotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiError(HttpStatus.NOT_FOUND.value(), "Not found", e.getMessage()));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiError(HttpStatus.NOT_FOUND.value(), "Not found"));
     }
     @ExceptionHandler(FeignException.NotFound.class)
     public ResponseEntity<ApiError> handleFeignNotFoundException(FeignException.NotFound e) {
@@ -19,7 +19,7 @@ public class ControllerAdvice {
     }
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ApiError> handleIllegalArgumentException(IllegalArgumentException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiError(HttpStatus.BAD_REQUEST.value(), "Bad request", e.getMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiError(HttpStatus.BAD_REQUEST.value(), "Bad request"));
     }
 
 }
--- src/main/java/com/thoroldvix/g2gcalculator/mapper/PriceMapper.java
+++ src/main/java/com/thoroldvix/g2gcalculator/mapper/PriceMapper.java
@@ -3,7 +3,10 @@ package com.thoroldvix.g2gcalculator.mapper;
 import com.thoroldvix.g2gcalculator.dto.PriceResponse;
 import com.thoroldvix.g2gcalculator.model.Price;
 import com.thoroldvix.g2gcalculator.model.Realm;
-import org.mapstruct.*;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.Named;
 
 import java.math.BigDecimal;
 
@@ -14,9 +17,10 @@ public interface PriceMapper {
     PriceResponse toPriceResponse(Price price);
 
     @Named(value = "mapPriceValue")
-   default String mapPriceValue(BigDecimal bigDecimal) {
+    default String mapPriceValue(BigDecimal bigDecimal) {
         return bigDecimal.multiply(BigDecimal.valueOf(1000)) + "/1k";
     }
+
     @Named(value = "mapRealmName")
     default String mapRealmName(Realm realm) {
         return realm.getName() + "-" + realm.getFaction();
