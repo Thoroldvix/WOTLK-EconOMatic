9bf04d71ff328d8bad0a99a345a4ed3c9eca0b55
Alexey <65923482+Thoroldvix@users.noreply.github.com>
2023-04-01 20:04:21 +0300

    add update price method

--- src/main/java/com/thoroldvix/g2gcalculator/price/PriceService.java
+++ src/main/java/com/thoroldvix/g2gcalculator/price/PriceService.java
@@ -1,18 +1,14 @@
 package com.thoroldvix.g2gcalculator.price;
 
-import com.thoroldvix.g2gcalculator.server.Server;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface PriceService {
-
-
-    PriceResponse getPriceForServerName(String realmName);
-
-    PriceResponse getPriceForServer(Server server);
+    <T> PriceResponse  getPriceForServer(T server);
 
     List<PriceResponse> getAllPricesForServer(String realmName, Pageable pageable);
 
+    void updatePrice(int serverId, PriceResponse recentPrice);
 
 }
