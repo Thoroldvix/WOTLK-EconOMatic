b3d32ef57d6d105912c3349595ca1aee0858e61c
Alexey <65923482+Thoroldvix@users.noreply.github.com>
2023-04-01 20:00:24 +0300

    add scheduled update for prices

--- src/main/java/com/thoroldvix/g2gcalculator/g2g/G2GService.java
+++ src/main/java/com/thoroldvix/g2gcalculator/g2g/G2GService.java
@@ -1,50 +1,79 @@
 package com.thoroldvix.g2gcalculator.g2g;
 
-import com.thoroldvix.g2gcalculator.common.NotFoundException;
-import com.thoroldvix.g2gcalculator.price.Price;
 import com.thoroldvix.g2gcalculator.price.PriceResponse;
-import com.thoroldvix.g2gcalculator.server.Server;
+import com.thoroldvix.g2gcalculator.price.PriceService;
+import com.thoroldvix.g2gcalculator.server.Region;
+import com.thoroldvix.g2gcalculator.server.ServerResponse;
+import com.thoroldvix.g2gcalculator.server.ServerService;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
+@EnableScheduling
 public class G2GService {
+    private static final String EU_REGION_ID = "ac3f85c1-7562-437e-b125-e89576b9a38e";
+    private static final String US_REGION_ID = "dfced32f-2f0a-4df5-a218-1e068cfadffa";
+
+    private int priceUpdateInterval;
     private final G2GPriceClient g2GPriceClient;
-    @Value("${g2g.currency:USD}")
-    private  String currency;
+
+    private final ServerService serverServiceImpl;
+
+    private final PriceService priceServiceImpl;
+
+    private final String currency = "USD";
 
 
     @SneakyThrows
-    public Price fetchServerPrice(Server server) {
-        String regionId = server.getRegion().getG2gId();
-        String serverName = formatServerName(server);
-
-        G2GPriceResponse response = g2GPriceClient.getPrices(regionId, currency);
-
-        PriceResponse price = findPriceInResponse(serverName, response);
-
-        return Price.builder()
-                .value(price.value().stripTrailingZeros())
-                .currency(currency)
-                .build();
+    @Scheduled(fixedRateString = "${g2g.price-update-interval:PT10M}", timeUnit = TimeUnit.MINUTES)
+    public void updateAllServerPrices() {
+        updateEUPrices();
+        updateUSPrices();
     }
 
-    private static PriceResponse findPriceInResponse(String serverName, G2GPriceResponse response) {
+    public void updateUSPrices() {
+        log.info("Updating US prices");
+        List<ServerResponse> usServers = serverServiceImpl.getAllForRegion(Region.getUSRegions());
+        G2GPriceListResponse usPrices = g2GPriceClient.getPrices(US_REGION_ID, currency);
+
+        usServers.forEach(server -> {
+            String serverName = formatServerName(server);
+            PriceResponse price = findPriceInResponse(serverName, usPrices);
+            priceServiceImpl.updatePrice(server.id(), price);
+        });
+        log.info("Finished updating US prices");
+    }
+
+    public void updateEUPrices() {
+        log.info("Updating EU prices");
+        List<ServerResponse> euServers = serverServiceImpl.getAllForRegion(Region.getEURegions());
+        G2GPriceListResponse euPrices = g2GPriceClient.getPrices(EU_REGION_ID, currency);
+
+        euServers.forEach(server -> {
+            String serverName = formatServerName(server);
+            PriceResponse price = findPriceInResponse(serverName, euPrices);
+            priceServiceImpl.updatePrice(server.id(), price);
+        });
+        log.info("Finished updating EU prices");
+    }
+
+    private PriceResponse findPriceInResponse(String serverName, G2GPriceListResponse response) {
         return response.prices().stream().filter(result -> result.serverName().equals(serverName))
                 .findFirst()
                 .orElseThrow(() -> new G2GPriceNotFoundException("Price not found for server: " + serverName));
     }
 
-    private String formatServerName(Server server) {
-        return String.format("%s [%s] - %s", server.getName(),
-                server.getRegion(), server.getFaction().toString());
+    private String formatServerName(ServerResponse server) {
+        return String.format("%s [%s] - %s", server.name(),
+                server.region(), server.faction().toString());
     }
 }
