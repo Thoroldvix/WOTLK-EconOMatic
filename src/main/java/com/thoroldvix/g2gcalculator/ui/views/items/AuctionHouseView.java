97c13a1114170af69e619c545a56f4b633b5f9d0
Alexey <xxxtrihardxxx@gmail.com>
2023-05-10 15:41:52 +0300

    rename ItemsView to AuctionHouseView

--- src/main/java/com/thoroldvix/g2gcalculator/ui/views/items/AuctionHouseView.java
+++ src/main/java/com/thoroldvix/g2gcalculator/ui/views/items/AuctionHouseView.java
@@ -2,34 +2,27 @@ package com.thoroldvix.g2gcalculator.ui.views.items;
 
 import com.thoroldvix.g2gcalculator.ui.views.MainLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
 
 @Route(value = "wow-classic/items", layout = MainLayout.class)
+@PageTitle("Auction House")
 @SpringComponent
 @UIScope
-public class ItemsView extends VerticalLayout {
-    private final ItemGridView  itemGridView;
-
-    private final ServerSelectionView serverSelectionView;
+public class AuctionHouseView extends VerticalLayout {
 
 
-    public ItemsView(
-            ItemGridView itemGridView, ServerSelectionView serverSelectionView) {
-
-
-
-        this.itemGridView = itemGridView;
-        this.serverSelectionView = serverSelectionView;
-
+    public AuctionHouseView(
+            ItemGridView itemGridView) {
 
         addClassName("items-view");
         setSizeFull();
         setSpacing(true);
         setAlignItems(Alignment.CENTER);
 
-        add(serverSelectionView, itemGridView);
+        add(itemGridView);
     }
 
 
