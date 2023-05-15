5f64e90945cf93596596b48b142d5ea5d5732a0a
Alexey <xxxtrihardxxx@gmail.com>
2023-05-15 20:24:55 +0300

    add item type filter

--- src/main/java/com/thoroldvix/g2gcalculator/ui/views/items/ItemFilteringLayout.java
+++ src/main/java/com/thoroldvix/g2gcalculator/ui/views/items/ItemFilteringLayout.java
@@ -1,11 +1,17 @@
 package com.thoroldvix.g2gcalculator.ui.views.items;
 
 import com.thoroldvix.g2gcalculator.item.ItemQuality;
+import com.thoroldvix.g2gcalculator.item.ItemType;
 import com.thoroldvix.g2gcalculator.item.dto.ItemInfo;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.combobox.MultiSelectComboBox;
+import com.vaadin.flow.component.combobox.MultiSelectComboBoxVariant;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.textfield.TextFieldVariant;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.data.value.ValueChangeMode;
 
 import java.util.function.Predicate;
@@ -14,34 +20,66 @@ import java.util.function.Predicate;
 public class ItemFilteringLayout extends HorizontalLayout {
     private TextField itemNameFilter;
     private MultiSelectComboBox<ItemQuality> itemQualityFilter;
-    private final ItemGridView itemGrid;
+
+    private MultiSelectComboBox<ItemType> itemTypeFilter;
+
+    private PriceRangeField priceRangeField = new PriceRangeField();
+    private final ItemGridLayout itemGrid;
 
 
-
-    public ItemFilteringLayout(ItemGridView itemGrid) {
+    public ItemFilteringLayout(ItemGridLayout itemGrid) {
         this.itemGrid = itemGrid;
         setVisible(false);
+        setAlignItems(Alignment.CENTER);
+        setVerticalComponentAlignment(Alignment.CENTER);
+        setJustifyContentMode(JustifyContentMode.START);
+        setWidthFull();
         prepareFilterFields();
-        add(itemNameFilter, itemQualityFilter);
+        add(itemNameFilter, itemQualityFilter, itemTypeFilter, priceRangeField);
     }
 
     private void prepareFilterFields() {
         itemNameFilter = getItemNameFilter();
         itemQualityFilter = getItemQualityFilter();
+        itemTypeFilter = getItemTypeFilter();
     }
 
     private MultiSelectComboBox<ItemQuality> getItemQualityFilter() {
         MultiSelectComboBox<ItemQuality> itemQualityFilter = new MultiSelectComboBox<>();
-        itemQualityFilter.setPlaceholder("Select item's quality...");
+        itemQualityFilter.addThemeVariants(MultiSelectComboBoxVariant.LUMO_HELPER_ABOVE_FIELD);
+        itemQualityFilter.setHelperText("Select Quality");
+        itemQualityFilter.setPlaceholder("Any");
         itemQualityFilter.setItems(ItemQuality.values());
-        itemQualityFilter.setItemLabelGenerator(ItemQuality::toString);
+        itemQualityFilter.setRenderer(new ComponentRenderer<Component, ItemQuality>(this::getItemQualityName));
+        itemQualityFilter.setClearButtonVisible(true);
         itemQualityFilter.addValueChangeListener(event -> itemGrid.onFilterChange(getFilter()));
         return itemQualityFilter;
     }
 
+    private Span getItemQualityName(ItemQuality itemQuality) {
+        Span itemQualityName = new Span(itemQuality.toString());
+        itemQualityName.setClassName(itemQuality.name().toLowerCase());
+        return itemQualityName;
+    }
+
+
+    private MultiSelectComboBox<ItemType> getItemTypeFilter() {
+        MultiSelectComboBox<ItemType> itemTypeFilter = new MultiSelectComboBox<>();
+        itemTypeFilter.setHelperText("Select Type");
+        itemTypeFilter.addThemeVariants(MultiSelectComboBoxVariant.LUMO_HELPER_ABOVE_FIELD);
+        itemTypeFilter.setPlaceholder("Any");
+        itemTypeFilter.setItems(ItemType.values());
+        itemTypeFilter.setClearButtonVisible(true);
+        itemTypeFilter.setItemLabelGenerator(ItemType::toString);
+        itemTypeFilter.addValueChangeListener(event -> itemGrid.onFilterChange(getFilter()));
+        return itemTypeFilter;
+    }
+
     private TextField getItemNameFilter() {
         TextField itemNameFilter = new TextField();
-        itemNameFilter.setPlaceholder("Filter by item name...");
+        itemNameFilter.addThemeVariants(TextFieldVariant.LUMO_HELPER_ABOVE_FIELD);
+        itemNameFilter.setPlaceholder("Search...");
+        itemNameFilter.setHelperText("Search for item");
         itemNameFilter.setValueChangeMode(ValueChangeMode.LAZY);
         itemNameFilter.setClearButtonVisible(true);
         itemNameFilter.addValueChangeListener(event -> itemGrid.onFilterChange(getFilter()));
@@ -52,13 +90,17 @@ public class ItemFilteringLayout extends HorizontalLayout {
         return itemInfo -> {
             boolean itemNameMatch = true;
             boolean itemQualityMatch = true;
+            boolean itemTypeMatch = true;
             if (!itemNameFilter.isEmpty()) {
                 itemNameMatch = itemInfo.name().toLowerCase().contains(itemNameFilter.getValue().toLowerCase());
             }
             if (!itemQualityFilter.getValue().isEmpty()) {
                 itemQualityMatch = itemQualityFilter.getValue().contains(itemInfo.quality());
             }
-            return itemNameMatch && itemQualityMatch;
+            if (!itemTypeFilter.getValue().isEmpty()) {
+                itemTypeMatch = itemTypeFilter.getValue().contains(itemInfo.type());
+            }
+            return itemNameMatch && itemQualityMatch && itemTypeMatch;
         };
     }
 }
