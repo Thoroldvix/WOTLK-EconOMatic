7818f157b4dd796877f990a200c4beaccdc1e3cc
Alexey <xxxtrihardxxx@gmail.com>
2023-05-15 20:28:03 +0300

    move server select to separate class

---
+++ src/main/java/com/thoroldvix/g2gcalculator/ui/views/ServerSelect.java
@@ -0,0 +1,28 @@
+package com.thoroldvix.g2gcalculator.ui.views;
+
+import com.thoroldvix.g2gcalculator.server.ServerResponse;
+import com.thoroldvix.g2gcalculator.server.ServerService;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.spring.annotation.SpringComponent;
+import com.vaadin.flow.spring.annotation.UIScope;
+
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+@UIScope
+@SpringComponent
+public class ServerSelect extends ComboBox<ServerResponse> {
+
+    public ServerSelect(ServerService serverServiceImpl) {
+        Set<ServerResponse> servers = new TreeSet<>(Comparator.comparing(ServerResponse::name));
+        servers.addAll(serverServiceImpl.getAllServers());
+        setItems(servers);
+        addClassName("server-select");
+        setAllowCustomValue(false);
+        setPlaceholder("Select Server");
+        setRequired(true);
+        setRequiredIndicatorVisible(true);
+        setItemLabelGenerator(ServerResponse::name);
+    }
+
+}
