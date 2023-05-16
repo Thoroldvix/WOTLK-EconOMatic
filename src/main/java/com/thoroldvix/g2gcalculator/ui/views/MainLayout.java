91462bbcb6b42ff6f0ed82a10e76fe8db9341e84
Alexey <xxxtrihardxxx@gmail.com>
2023-05-16 20:45:39 +0300

    add search field to main layout navbar

--- src/main/java/com/thoroldvix/g2gcalculator/ui/views/MainLayout.java
+++ src/main/java/com/thoroldvix/g2gcalculator/ui/views/MainLayout.java
@@ -1,7 +1,7 @@
 package com.thoroldvix.g2gcalculator.ui.views;
 
 import com.thoroldvix.g2gcalculator.ui.views.items.AuctionHouseView;
-import com.thoroldvix.g2gcalculator.ui.views.items.ItemOverview;
+import com.thoroldvix.g2gcalculator.ui.views.items.ItemsLayout;
 import com.thoroldvix.g2gcalculator.ui.views.items.ServerSelectionField;
 import com.thoroldvix.g2gcalculator.ui.views.servers.ServerGridView;
 import com.vaadin.flow.component.Component;
@@ -9,16 +9,19 @@ import com.vaadin.flow.component.ComponentUtil;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.component.tabs.TabsVariant;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLink;
 
+import java.awt.*;
 import java.util.Optional;
 
 @Route("wow-classic")
@@ -27,90 +30,90 @@ public class MainLayout extends AppLayout {
     private final Tabs menu;
     private final ServerSelectionField serverSelectionField;
 
-    private HorizontalLayout headerLayout;
-    private H1 viewTitle;
+    private HorizontalLayout headerLayout = new HorizontalLayout();
+
+    private TextField itemSearchBar = new TextField();
 
 
     public MainLayout(ServerSelectionField serverSelectionField) {
         this.serverSelectionField = serverSelectionField;
 
         menu = createMenu();
-        setPrimarySection(Section.DRAWER);
-        addToNavbar(true, createHeaderContent());
+        setPrimarySection(Section.NAVBAR);
+        configureItemSearchBar();
+        addToNavbar(true, createLogoLayout(), createHeaderContent());
 
         addToDrawer(createDrawerContent(menu));
     }
 
+    private void configureItemSearchBar() {
+        itemSearchBar.setWidthFull();
+        itemSearchBar.setPlaceholder("Search items...");
+        itemSearchBar.setPrefixComponent(VaadinIcon.SEARCH.create());
+    }
 
     private Component createHeaderContent() {
-        headerLayout = new HorizontalLayout();
-
-
-        headerLayout.setId("header");
-        headerLayout.getThemeList().set("dark", true);
-        headerLayout.setWidthFull();
-        headerLayout.setSpacing(false);
-        headerLayout.getThemeList().set("spacing-s", true);
-        headerLayout.setPadding(false);
-        headerLayout.setAlignItems(FlexComponent.Alignment.CENTER);
-        headerLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.BETWEEN);
-
-
-        viewTitle = new H1();
-        viewTitle.getStyle().set("font-size", "var(--lumo-font-size-l)")
-                .set("margin", "0");
-        HorizontalLayout titleLayout = new HorizontalLayout(new DrawerToggle(), viewTitle);
-        titleLayout.setWidthFull();
-        titleLayout.setSpacing(false);
-        titleLayout.setAlignItems(FlexComponent.Alignment.CENTER);
-
-        headerLayout.add(titleLayout);
+        configureHeaderLayout();
+        headerLayout.add(itemSearchBar, serverSelectionField);
 
         return headerLayout;
     }
 
+    private HorizontalLayout createLogoLayout() {
+        HorizontalLayout logoLayout = new HorizontalLayout();
+        logoLayout.setId("logo");
+        logoLayout.setSpacing(false);
+        logoLayout.setMargin(false);
+        logoLayout.setPadding(false);
+        logoLayout.getThemeList().set("padding-s", true);
+        logoLayout.getThemeList().set("spacing-s", true);
+
+        logoLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+
+        H1 appTitle = new H1("PricePal");
+        appTitle.getStyle().set("font-size", "var(--lumo-font-size-l)")
+                .set("font-weight", "bold")
+                .set("line-height", "var(--lumo-size-l)");
+
+
+        logoLayout.add(new DrawerToggle(), appTitle);
+
+
+        return logoLayout;
+    }
+
+    private void configureHeaderLayout() {
+        headerLayout.setId("header");
+        headerLayout.setWidth("50%");
+        headerLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+        headerLayout.setSpacing(false);
+        headerLayout.setMargin(false);
+        headerLayout.getThemeList().set("spacing-s", true);
+        headerLayout.getThemeList().set("margin-s", true);
+        headerLayout.getStyle().set("margin", "auto");
+        headerLayout.setPadding(false);
+    }
+
     private Component createDrawerContent(Tabs menu) {
         VerticalLayout layout = new VerticalLayout();
-
-
         layout.setSizeFull();
         layout.setPadding(false);
         layout.setSpacing(false);
         layout.getThemeList().set("spacing-s", true);
         layout.setAlignItems(FlexComponent.Alignment.STRETCH);
-
-        // Have a drawer header with an application logo
-        HorizontalLayout logoLayout = new HorizontalLayout();
-        logoLayout.setId("logo");
-        logoLayout.setAlignItems(FlexComponent.Alignment.CENTER);
-
-        H1 appTitle = new H1("PricePal");
-        appTitle.getStyle().set("font-size", "var(--lumo-font-size-l)")
-                .set("line-height", "var(--lumo-size-l)")
-                .set("margin", "0 var(--lumo-space-m)");
-
-        logoLayout.add(appTitle);
-
-
-        layout.add(logoLayout, menu);
+        layout.add(menu);
         return layout;
     }
 
     @Override
     protected void afterNavigation() {
         super.afterNavigation();
-
-
         getTabForComponent(getContent()).ifPresent(menu::setSelectedTab);
 
-
-        viewTitle.setText(getCurrentPageTitle());
-
-        if (getContent().getClass().equals(AuctionHouseView.class) || getContent().getClass().equals(ItemOverview.class)) {
-            headerLayout.add(serverSelectionField);
-        }
-         else {
+        if (getContent().getClass().equals(ServerGridView.class)) {
             headerLayout.remove(serverSelectionField);
+        } else {
+            headerLayout.add(serverSelectionField);
         }
     }
 
@@ -137,7 +140,7 @@ public class MainLayout extends AppLayout {
     private Tab[] createMenuItems() {
         return new Tab[]{createTab("G2G Prices", ServerGridView.class),
                 createTab("Auction House", AuctionHouseView.class),
-                createTab("Item Overview", ItemOverview.class)
+                createTab("Items", ItemsLayout.class)
         };
     }
 
