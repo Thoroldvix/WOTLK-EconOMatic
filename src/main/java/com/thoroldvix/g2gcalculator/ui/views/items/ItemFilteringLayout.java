8c9b96bf1deea3ece058f670d41cd127510bc3f8
Alexey <xxxtrihardxxx@gmail.com>
2023-05-16 20:45:56 +0300

    add icon to search field

--- src/main/java/com/thoroldvix/g2gcalculator/ui/views/items/ItemFilteringLayout.java
+++ src/main/java/com/thoroldvix/g2gcalculator/ui/views/items/ItemFilteringLayout.java
@@ -8,6 +8,7 @@ import com.vaadin.flow.component.combobox.MultiSelectComboBox;
 import com.vaadin.flow.component.combobox.MultiSelectComboBoxVariant;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.textfield.TextFieldVariant;
@@ -77,7 +78,8 @@ public class ItemFilteringLayout extends HorizontalLayout {
         TextField itemNameFilter = new TextField();
         itemNameFilter.addThemeVariants(TextFieldVariant.LUMO_HELPER_ABOVE_FIELD);
         itemNameFilter.setPlaceholder("Search...");
-        itemNameFilter.setHelperText("Search for item");
+        itemNameFilter.setPrefixComponent(VaadinIcon.SEARCH.create());
+        itemNameFilter.setHelperText("Search items...");
         itemNameFilter.setValueChangeMode(ValueChangeMode.LAZY);
         itemNameFilter.setClearButtonVisible(true);
         itemNameFilter.addValueChangeListener(event -> itemGrid.onFilterChange(getFilter()));
