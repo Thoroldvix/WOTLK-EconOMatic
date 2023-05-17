442ed1342166661b91d6539856786ee9bc6d4b28
Alexey <xxxtrihardxxx@gmail.com>
2023-05-17 16:23:07 +0300

    change server selection to store selected server in session

--- src/main/java/com/thoroldvix/g2gcalculator/ui/views/items/ServerSelectionField.java
+++ src/main/java/com/thoroldvix/g2gcalculator/ui/views/items/ServerSelectionField.java
@@ -1,28 +1,34 @@
 package com.thoroldvix.g2gcalculator.ui.views.items;
 
-import com.thoroldvix.g2gcalculator.server.Faction;
 import com.thoroldvix.g2gcalculator.server.ServerResponse;
 import com.thoroldvix.g2gcalculator.ui.views.FactionSelect;
 import com.thoroldvix.g2gcalculator.ui.views.ServerSelect;
+import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.DetachEvent;
 import com.vaadin.flow.component.customfield.CustomField;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.server.VaadinSession;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
 
 @SpringComponent
 @UIScope
 public class ServerSelectionField extends CustomField<ServerResponse> {
-    private final FactionSelect factionSelect = new FactionSelect();
+    private final FactionSelect factionSelect;
     private final ServerSelect serverSelect;
 
-    public ServerSelectionField(ServerSelect serverSelect, ItemGridLayout itemGrid) {
+
+    public ServerSelectionField(FactionSelect factionSelect, ServerSelect serverSelect, ItemGridLayout itemGridLayout) {
+        this.factionSelect = factionSelect;
         this.serverSelect = serverSelect;
+
         addValueChangeListener(event -> {
             if (event.getValue().faction() != null && event.getValue().name() != null) {
-                itemGrid.populateGrid(getServerName(event.getValue().name(),  event.getValue().faction()));
+                itemGridLayout.populateGridForServer(event.getValue());
             }
-
         });
+
+
         configureFactionSelect();
         HorizontalLayout layout = new HorizontalLayout(serverSelect, factionSelect);
         add(layout);
@@ -45,7 +51,21 @@ public class ServerSelectionField extends CustomField<ServerResponse> {
         factionSelect.setValue(newPresentationValue.faction());
         serverSelect.setValue(newPresentationValue);
     }
-    private String getServerName(String serverName, Faction faction) {
-        return serverName.replaceAll(" ", "-").toLowerCase() + "-" + faction.toString().toLowerCase();
+
+
+    @Override
+    protected void onAttach(AttachEvent attachEvent) {
+        super.onAttach(attachEvent);
+        ServerResponse selectedServer = (ServerResponse) VaadinSession.getCurrent().getAttribute("selectedServer");
+
+        setValue(selectedServer);
+    }
+
+    @Override
+    protected void onDetach(DetachEvent detachEvent) {
+        super.onDetach(detachEvent);
+        ServerResponse selectedServer = getValue();
+
+        VaadinSession.getCurrent().setAttribute("selectedServer", selectedServer);
     }
 }
