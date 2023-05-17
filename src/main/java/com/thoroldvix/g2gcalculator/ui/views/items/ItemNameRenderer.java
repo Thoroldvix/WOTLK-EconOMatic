7b6c722a7aa4bf6edd583763243402250787d532
Alexey <xxxtrihardxxx@gmail.com>
2023-05-17 16:18:28 +0300

    add item type to item name renderer

--- src/main/java/com/thoroldvix/g2gcalculator/ui/views/items/ItemNameRenderer.java
+++ src/main/java/com/thoroldvix/g2gcalculator/ui/views/items/ItemNameRenderer.java
@@ -5,6 +5,7 @@ import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 @CssImport("./styles/shared-styles.css")
 public class ItemNameRenderer extends HorizontalLayout {
@@ -24,12 +25,27 @@ public class ItemNameRenderer extends HorizontalLayout {
     private void configureItemIconDisplay() {
         Image itemIcon = new Image(itemInfo.icon(), "icon");
         itemIcon.setWidth("40px");
+        itemIcon.setHeight("40px");
         add(itemIcon);
     }
 
     private void configureItemNameDisplay() {
+        VerticalLayout itemNameLayout = new VerticalLayout();
+        itemNameLayout.setAlignItems(Alignment.START);
+        itemNameLayout.setWidthFull();
+        itemNameLayout.setPadding(false);
+        itemNameLayout.setSpacing(false);
+        itemNameLayout.setMargin(false);
+
         Span name = new Span(itemInfo.name());
+        Span type = new Span(itemInfo.type().toString());
+
         name.getClassNames().add(itemInfo.quality().name().toLowerCase());
-        add(name);
+        type.getStyle().set("font-size", "--lumo-font-size-xs");
+        type.getStyle().set("color", "--lumo-secondary-text-color");
+
+
+        itemNameLayout.add(name, type);
+        add(itemNameLayout);
     }
 }
