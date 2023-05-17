a5e97aa39ba04b438d3d3aa5a5b49c101ee12b40
Alexey <xxxtrihardxxx@gmail.com>
2023-05-17 16:17:23 +0300

    convert FactionSelect component to spring bean

--- src/main/java/com/thoroldvix/g2gcalculator/ui/views/FactionSelect.java
+++ src/main/java/com/thoroldvix/g2gcalculator/ui/views/FactionSelect.java
@@ -3,11 +3,12 @@ package com.thoroldvix.g2gcalculator.ui.views;
 import com.thoroldvix.g2gcalculator.server.Faction;
 import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
+import com.vaadin.flow.spring.annotation.SpringComponent;
+import com.vaadin.flow.spring.annotation.UIScope;
 
+@SpringComponent
+@UIScope
 public class FactionSelect extends Select<Faction> {
-
-
-
     public FactionSelect() {
         addClassName("faction-select");
         setPlaceholder("Select Faction");
