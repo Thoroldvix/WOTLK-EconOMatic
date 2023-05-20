252e82dfbac0301d2f600e1b2ea9c3de5b47262e
Alexey <xxxtrihardxxx@gmail.com>
2023-05-20 20:38:59 +0300

    fix import

--- src/test/java/com/thoroldvix/g2gcalculator/server/ClassicServerControllerTest.java
+++ src/test/java/com/thoroldvix/g2gcalculator/server/ClassicServerControllerTest.java
@@ -1,7 +1,7 @@
 package com.thoroldvix.g2gcalculator.server;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.thoroldvix.g2gcalculator.common.NotFoundException;
+import com.vaadin.flow.router.NotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
