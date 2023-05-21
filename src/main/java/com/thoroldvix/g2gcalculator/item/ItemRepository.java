8653b659166d604550f716387c2cf2e9c8b1a9f1
Alexey <xxxtrihardxxx@gmail.com>
2023-05-21 22:49:45 +0300

    add methods for finding item by name and searching item by name

--- src/main/java/com/thoroldvix/g2gcalculator/item/ItemRepository.java
+++ src/main/java/com/thoroldvix/g2gcalculator/item/ItemRepository.java
@@ -1,8 +1,19 @@
 package com.thoroldvix.g2gcalculator.item;
 
+import com.thoroldvix.g2gcalculator.item.dto.ItemInfo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Integer> {
+
+    Optional<Item> findByName(String itemName);
+
+    @Query("select i from Item i where i.name like %?1%")
+    Page<ItemInfo> searchItemsByName(String query, Pageable pageable);
 }
--- src/main/java/com/thoroldvix/g2gcalculator/item/dto/ItemInfo.java
+++ src/main/java/com/thoroldvix/g2gcalculator/item/dto/ItemInfo.java
@@ -1,31 +1,18 @@
 package com.thoroldvix.g2gcalculator.item.dto;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.thoroldvix.g2gcalculator.item.ItemQuality;
 import com.thoroldvix.g2gcalculator.item.ItemType;
 import lombok.Builder;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-
 @Builder
-@JsonDeserialize(using = ItemInfoDeserializer.class)
 public record ItemInfo(
-        String server,
+        int id,
         ItemType type,
         ItemQuality quality,
         String icon,
-        BigDecimal price,
-        String currency,
-        String name,
-        AuctionHouseInfo auctionHouseInfo,
-        LocalDateTime lastUpdated
-
+        String name
 ) {
     public String getFormattedItemName() {
         return name.replaceAll("\\s+|(:\\s+)", "-").toLowerCase();
-    }
-    public String getWowheadUrl() {
-        return String.format("https://www.wowhead.com/wotlk/item=%d", auctionHouseInfo.itemId());
     }
 }
