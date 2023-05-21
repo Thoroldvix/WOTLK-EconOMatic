4ecbedef66f1260f0a3fd40cb5cb2856bd446f8c
Alexey <xxxtrihardxxx@gmail.com>
2023-05-21 22:39:54 +0300

    remove fields

--- src/main/java/com/thoroldvix/g2gcalculator/item/dto/AuctionHouseInfo.java
+++ src/main/java/com/thoroldvix/g2gcalculator/item/dto/AuctionHouseInfo.java
@@ -2,13 +2,17 @@ package com.thoroldvix.g2gcalculator.item.dto;
 
 import lombok.Builder;
 
+import java.time.LocalDateTime;
+
 @Builder
 public record AuctionHouseInfo(
         int itemId,
+        String server,
         long minBuyout,
         long marketValue,
         int quantity,
-        int numAuctions
+        int numAuctions,
+        LocalDateTime lastUpdated
 ) {
 
 }
