d7d04411e5d617c18b1f2542bd2b61ed913a2c66
Alexey <xxxtrihardxxx@gmail.com>
2023-05-21 22:55:18 +0300

    add methods for searching items by name, and batch finding items by ids

--- src/main/java/com/thoroldvix/g2gcalculator/item/ItemService.java
+++ src/main/java/com/thoroldvix/g2gcalculator/item/ItemService.java
@@ -1,20 +1,22 @@
 package com.thoroldvix.g2gcalculator.item;
 
 import com.thoroldvix.g2gcalculator.item.dto.ItemInfo;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 
+
+import java.util.List;
 import java.util.Set;
 
 public interface ItemService {
 
-    ItemInfo getItemByName(String serverName, String itemName);
-
-    ItemInfo getItemById(String serverName, int itemId);
-
-    Set<ItemInfo> getAllItemsInfoForServer(String serverName);
-
-
-    Set<ItemInfo> getAllItemsInfo();
+    ItemInfo getItemByName(String itemName);
+    ItemInfo getItemById(int itemId);
+    Set<ItemInfo> getAllItems(Pageable pageable);
     void saveItem(Item item);
-
     long getItemCount();
+
+    List<ItemInfo> findItemsByIds(List<Integer> ids);
+
+    List<ItemInfo> searchItems(String query, Pageable pageable);
 }
