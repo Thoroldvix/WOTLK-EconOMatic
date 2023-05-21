eb95f62d639c8811cc65eb046afce3c06290541d
Alexey <xxxtrihardxxx@gmail.com>
2023-05-21 22:43:34 +0300

    remove services that I decided no longer needed in app

--- src/main/java/com/thoroldvix/g2gcalculator/item/price/RMItemPriceCalculator.java
+++
@@ -1,9 +0,0 @@
-package com.thoroldvix.g2gcalculator.item.price;
-
-import com.thoroldvix.g2gcalculator.price.PriceResponse;
-
-import java.math.BigDecimal;
-
-public interface RMItemPriceCalculator {
-    BigDecimal calculatePrice(long targetPrice, PriceResponse price, int amount);
-}
