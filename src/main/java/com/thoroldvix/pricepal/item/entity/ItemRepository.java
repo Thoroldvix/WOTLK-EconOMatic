c45ddc64f857d23ea94aeeb6e75ed834b685bd01
Alexey <xxxtrihardxxx@gmail.com>
2023-05-24 17:28:53 +0300

    add method for finding item by its unique name

--- src/main/java/com/thoroldvix/pricepal/item/entity/ItemRepository.java
+++ src/main/java/com/thoroldvix/pricepal/item/entity/ItemRepository.java
@@ -1,6 +1,6 @@
-package com.thoroldvix.g2gcalculator.item.entity;
+package com.thoroldvix.pricepal.item.entity;
 
-import com.thoroldvix.g2gcalculator.item.dto.ItemInfo;
+import com.thoroldvix.pricepal.item.dto.ItemInfo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,6 +13,8 @@ import java.util.Optional;
 public interface ItemRepository extends JpaRepository<Item, Integer> {
 
     Optional<Item> findByName(String itemName);
+
+    Optional<Item> findByUniqueName(String uniqueName);
 
     @Query("select i from Item i where i.name like %?1%")
     Page<ItemInfo> searchItemsByName(String query, Pageable pageable);
