49624a93c84a683e508417b702139a6bc4fe32a5
Alexey <xxxtrihardxxx@gmail.com>
2023-05-27 23:20:23 +0300

    remove redundant regions

--- src/main/java/com/thoroldvix/pricepal/server/entity/Region.java
+++ src/main/java/com/thoroldvix/pricepal/server/entity/Region.java
@@ -2,32 +2,11 @@ package com.thoroldvix.pricepal.server.entity;
 
 import lombok.Getter;
 
-import java.util.List;
-
 @Getter
 public enum Region {
     EU("ac3f85c1-7562-437e-b125-e89576b9a38e"),
-    US(("dfced32f-2f0a-4df5-a218-1e068cfadffa")),
-    FR("ac3f85c1-7562-437e-b125-e89576b9a38e"),
-    DE("ac3f85c1-7562-437e-b125-e89576b9a38e"),
-    ES("ac3f85c1-7562-437e-b125-e89576b9a38e"),
-    OCE("dfced32f-2f0a-4df5-a218-1e068cfadffa"),
-    RU("166fbf02-6d9a-45a0-9f74-ac3ba5a002b4");
-
-    public static List<Region> getEURegions() {
-        return List.of(EU, FR, DE, ES, RU);
-    }
-
-    public static List<Region> getUSRegions() {
-        return List.of(US, OCE);
-    }
-
-    public Region getParentRegion() {
-        return switch (this) {
-            case EU, FR, ES, RU, DE -> EU;
-            case US, OCE -> US;
-        };
-    }
+    RU("166fbf02-6d9a-45a0-9f74-ac3ba5a002b4"),
+    US(("dfced32f-2f0a-4df5-a218-1e068cfadffa"));
 
     public static boolean contains(String region) {
         for (Region r : Region.values()) {
