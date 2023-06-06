ce6fe740f1aa273b181f7cf317abf04051422ee4
Alexey <xxxtrihardxxx@gmail.com>
2023-06-06 19:19:02 +0300

    refactor to use switch statement

--- src/main/java/com/thoroldvix/pricepal/common/service/SearchSpecification.java
+++ src/main/java/com/thoroldvix/pricepal/common/service/SearchSpecification.java
@@ -2,7 +2,6 @@ package com.thoroldvix.pricepal.common.service;
 
 import com.thoroldvix.pricepal.common.dto.RequestDto;
 import com.thoroldvix.pricepal.common.dto.SearchCriteria;
-import com.thoroldvix.pricepal.common.util.ValidationUtils;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.Path;
 import jakarta.persistence.criteria.Predicate;
@@ -13,28 +12,64 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+
+import static com.thoroldvix.pricepal.common.util.ValidationUtils.*;
 
 @Service
 public class SearchSpecification<T> {
-    public Specification<T> getSearchSpecification(List<SearchCriteria> searchCriteria, RequestDto.GlobalOperator globalOperator) {
-        ValidationUtils.validateListNotNullOrEmpty(searchCriteria,
-                () -> new IllegalArgumentException("Search criteria list cannot be null or empty."));
+    public Specification<T> getSearchSpecification(List<SearchCriteria> searchCriteria, RequestDto.GlobalOperator globalOperator,
+                                                   String joinTable) {
+        if (searchCriteria == null || searchCriteria.isEmpty()) {
+            return (root, query, cb) -> cb.isTrue(cb.literal(true));
+        }
         return (root, query, cb) -> {
             List<Predicate> predicates = new ArrayList<>();
             for (SearchCriteria criteria : searchCriteria) {
-                Path<?> columnPath = getColumnPath(root, criteria);
+                Path<?> columnPath = getColumnPath(root, criteria, joinTable);
                 Predicate predicate = getPredicateFromOperation(cb, criteria, columnPath);
                 predicates.add(predicate);
             }
             if (globalOperator == null || globalOperator.equals(RequestDto.GlobalOperator.AND)) {
                 return cb.and(predicates.toArray(new Predicate[0]));
-            }
-            else if (globalOperator.equals(RequestDto.GlobalOperator.OR)) {
+            } else if (globalOperator.equals(RequestDto.GlobalOperator.OR)) {
                 return cb.or(predicates.toArray(new Predicate[0]));
             }
             throw new IllegalArgumentException("Invalid global operator: " + globalOperator);
         };
+    }
+
+    public Specification<T> getJoinSpecForServerIdentifier(String serverIdentifier) {
+        if (isNumber(serverIdentifier)) {
+            int serverId = Integer.parseInt(serverIdentifier);
+            validatePositiveInt(serverId, "Server id must be positive");
+            return getJoinSpecForServerId(serverId);
+        } else {
+            return getJoinSpecForServerUniqueName(serverIdentifier);
+        }
+    }
+
+    public Specification<T> getJoinSpecForServerId(int serverId) {
+        SearchCriteria searchCriteria = SearchCriteria.builder()
+                .column("id")
+                .value(String.valueOf(serverId))
+                .operation(SearchCriteria.Operation.EQUALS)
+                .build();
+
+        return getSearchSpecification(Collections.singletonList(searchCriteria),
+                RequestDto.GlobalOperator.AND,
+                "server");
+    }
+
+    public Specification<T> getJoinSpecForServerUniqueName(String uniqueName) {
+        SearchCriteria searchCriteria = SearchCriteria.builder()
+                .column("uniqueName")
+                .value(uniqueName)
+                .operation(SearchCriteria.Operation.EQUALS)
+                .build();
+
+        return getSearchSpecification(Collections.singletonList(searchCriteria), RequestDto.GlobalOperator.AND, "server");
     }
 
     private Predicate getPredicateFromOperation(CriteriaBuilder cb, SearchCriteria searchCriteria, Path<?> columnPath) {
@@ -60,33 +95,15 @@ public class SearchSpecification<T> {
         }
         String lowerBound = split[0];
         String upperBound = split[1];
-        if (Integer.class.isAssignableFrom(columnPath.getJavaType())) {
-            Integer lowerIntegerBound = Integer.parseInt(lowerBound);
-            Integer upperIntegerBound = Integer.parseInt(upperBound);
-            return cb.between(columnPath.as(Integer.class), lowerIntegerBound, upperIntegerBound);
-        } else if (Long.class.isAssignableFrom(columnPath.getJavaType())) {
-            Long lowerLongBound = Long.parseLong(lowerBound);
-            Long upperLongBound = Long.parseLong(upperBound);
-            return cb.between(columnPath.as(Long.class), lowerLongBound, upperLongBound);
-        } else if (Double.class.isAssignableFrom(columnPath.getJavaType())) {
-            Double lowerDoubleBound = Double.parseDouble(lowerBound);
-            Double upperDoubleBound = Double.parseDouble(upperBound);
-            return cb.between(columnPath.as(Double.class), lowerDoubleBound, upperDoubleBound);
-        } else if (LocalDateTime.class.isAssignableFrom(columnPath.getJavaType())) {
-            LocalDateTime lowerDateBound = convertToLocalDateTime(lowerBound);
-            LocalDateTime upperDateBound = convertToLocalDateTime(upperBound);
-
-            return cb.between(columnPath.as(LocalDateTime.class), lowerDateBound, upperDateBound);
-        }
-        throw new IllegalArgumentException("Invalid operation: BETWEEN is only applicable to numeric and date-time column types.");
-    }
-
-    private LocalDateTime convertToLocalDateTime(String lowerBound) {
-        try {
-            return LocalDateTime.parse(lowerBound);
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Invalid operation: BETWEEN requires 2 values.");
-        }
+        Class<?> columnType = columnPath.getJavaType();
+        return switch (columnType.getSimpleName()) {
+            case "Integer" -> getIntegerBetweenPredicate(cb, columnPath, lowerBound, upperBound);
+            case "Long" -> getLongBetweenPredicate(cb, columnPath, lowerBound, upperBound);
+            case "Double" -> getDoubleBetweenPredicate(cb, columnPath, lowerBound, upperBound);
+            case "LocalDateTime" -> getLocalDateTimeBetweenPredicate(cb, columnPath, lowerBound, upperBound);
+            default ->
+                    throw new IllegalArgumentException("Invalid operation: BETWEEN is only applicable to numeric and date-time column types.");
+        };
     }
 
     private Predicate getEqualsIgnoreCasePredicate(CriteriaBuilder cb, Path<?> columnPath, String value) {
@@ -121,46 +138,55 @@ public class SearchSpecification<T> {
     }
 
     private Predicate getLessThanPredicate(CriteriaBuilder cb, Path<?> columnPath, String value) {
-        if (Integer.class.isAssignableFrom(columnPath.getJavaType())) {
-            Integer numericValue = Integer.parseInt(value);
-            return cb.lessThan(columnPath.as(Integer.class), numericValue);
-        } else if (Long.class.isAssignableFrom(columnPath.getJavaType())) {
-            Long numericValue = Long.parseLong(value);
-            return cb.lessThan(columnPath.as(Long.class), numericValue);
-        } else if (Double.class.isAssignableFrom(columnPath.getJavaType())) {
-            Double numericValue = Double.parseDouble(value);
-            return cb.lessThan(columnPath.as(Double.class), numericValue);
-        }
-        throw new IllegalArgumentException("Invalid operation: LESS_THAN is only applicable to numeric  column types.");
+        Class<?> columnType = columnPath.getJavaType();
+        return switch (columnType.getSimpleName()) {
+            case "Integer" -> cb.lessThan(columnPath.as(Integer.class), Integer.parseInt(value));
+            case "Long" -> cb.lessThan(columnPath.as(Long.class), Long.parseLong(value));
+            case "Double" -> cb.lessThan(columnPath.as(Double.class), Double.parseDouble(value));
+            case "LocalDateTime" -> cb.lessThan(columnPath.as(LocalDateTime.class), LocalDateTime.parse(value));
+            default ->
+                    throw new IllegalArgumentException("Invalid operation: LESS_THAN is only applicable to numeric and date-time column types.");
+        };
     }
+
 
     private Predicate getGreaterThanPredicate(CriteriaBuilder cb, Path<?> columnPath, String value) {
-        if (Integer.class.isAssignableFrom(columnPath.getJavaType())) {
-            Integer numericValue = Integer.parseInt(value);
-            return cb.greaterThan(columnPath.as(Integer.class), numericValue);
-        } else if (Long.class.isAssignableFrom(columnPath.getJavaType())) {
-            Long numericValue = Long.parseLong(value);
-            return cb.greaterThan(columnPath.as(Long.class), numericValue);
-        } else if (Double.class.isAssignableFrom(columnPath.getJavaType())) {
-            Double numericValue = Double.parseDouble(value);
-            return cb.greaterThan(columnPath.as(Double.class), numericValue);
-        } else if (LocalDateTime.class.isAssignableFrom(columnPath.getJavaType())) {
-            LocalDateTime currentDate = LocalDateTime.now();
-            int daysAgo = Integer.parseInt(value);
-            LocalDateTime targetDate = currentDate.minusDays(daysAgo);
-            return cb.greaterThan(columnPath.as(LocalDateTime.class), targetDate);
-        }
-        throw new IllegalArgumentException("Invalid operation: GREATER_THAN is only applicable to numeric and datetime column types.");
+        Class<?> columnType = columnPath.getJavaType();
+        return switch (columnType.getSimpleName()) {
+            case "Integer" -> cb.greaterThan(columnPath.as(Integer.class), Integer.parseInt(value));
+            case "Long" -> cb.greaterThan(columnPath.as(Long.class), Long.parseLong(value));
+            case "Double" -> cb.greaterThan(columnPath.as(Double.class), Double.parseDouble(value));
+            case "LocalDateTime" -> cb.greaterThan(columnPath.as(LocalDateTime.class), LocalDateTime.parse(value));
+            default ->
+                    throw new IllegalArgumentException("Invalid operation: GREATER_THAN is only applicable to numeric and date-time column types.");
+        };
     }
 
-    private Path<?> getColumnPath(Root<T> root, SearchCriteria searchCriteria) {
-        if (searchCriteria.joinOperation()) {
-            ValidationUtils.validateNonEmptyString(searchCriteria.joinTable(), "You must provide valid join table");
-            return root.join(searchCriteria.joinTable()).get(searchCriteria.column());
+    private Path<?> getColumnPath(Root<T> root, SearchCriteria searchCriteria, String joinTable) {
+        if (searchCriteria == null) {
+            throw new IllegalArgumentException("You must provide valid search criteria");
+        }
+        if (hasText(joinTable)) {
+            return root.join(joinTable).get(searchCriteria.column());
         }
         return root.get(searchCriteria.column());
     }
 
+    private Predicate getLocalDateTimeBetweenPredicate(CriteriaBuilder cb, Path<?> columnPath, String lowerBound, String upperBound) {
+        return cb.between(columnPath.as(LocalDateTime.class), LocalDateTime.parse(lowerBound), LocalDateTime.parse(upperBound));
+    }
+
+    private Predicate getDoubleBetweenPredicate(CriteriaBuilder cb, Path<?> columnPath, String lowerBound, String upperBound) {
+        return cb.between(columnPath.as(Double.class), Double.parseDouble(lowerBound), Double.parseDouble(upperBound));
+    }
+
+    private Predicate getLongBetweenPredicate(CriteriaBuilder cb, Path<?> columnPath, String lowerBound, String upperBound) {
+        return cb.between(columnPath.as(Long.class), Long.parseLong(lowerBound), Long.parseLong(upperBound));
+    }
+
+    private Predicate getIntegerBetweenPredicate(CriteriaBuilder cb, Path<?> columnPath, String lowerBound, String upperBound) {
+        return cb.between(columnPath.as(Integer.class), Integer.parseInt(lowerBound), Integer.parseInt(upperBound));
+    }
 }
 
 
