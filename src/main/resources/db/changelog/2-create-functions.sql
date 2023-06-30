000fdb05d924472c965414a479ddef3644b85ebe
Alexey <xxxtrihardxxx@gmail.com>
2023-06-30 19:35:17 +0300

    Update dependencies and modify database functions
    Updated to the latest versions of several build.gradle dependencies for better performance and bug fixes. Removed unused function 'sum_if_equals' from '2-create-functions.sql' as it is no longer needed in our use case.
    Renamed certain entities in docker-compose.yml to reflect new service name 'economatic'. This was necessary to keep the consistency and ease any future references to these services.

--- src/main/resources/db/changelog/2-create-functions.sql
+++ src/main/resources/db/changelog/2-create-functions.sql
@@ -6,11 +6,3 @@ CREATE OR REPLACE FUNCTION trigger_set_timestamp()
         RETURN NEW;
     END;
 ' LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION sum_if_equals(field INT, target INT)
-    RETURNS INT AS
-'
-    BEGIN
-        RETURN SUM(CASE WHEN field = target THEN 1 ELSE 0 END);
-    END;
-' LANGUAGE plpgsql;
