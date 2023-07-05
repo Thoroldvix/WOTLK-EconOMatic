fe1abc869a9b6ecaaa431ef0fa9c3728ec5667b8
Alexey <xxxtrihardxxx@gmail.com>
2023-07-05 18:27:25 +0300

    enable gradle caching

--- src/main/java/com/thoroldvix/economatic/EconOMaticApplication.java
+++ src/main/java/com/thoroldvix/economatic/EconOMaticApplication.java
@@ -13,7 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableRetry
 @ConfigurationPropertiesScan
 public class EconOMaticApplication {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         SpringApplication.run(EconOMaticApplication.class, args);
     }
 }
